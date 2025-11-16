03c864c5ddeb6241678404254e8cd780064bcd21
Manuel Gomez <Manuel.gomez2@student.uagc.edu>
2025-11-16 18:48:31 -0500

    Clean push for Final Paper Part 2 - removed node_modules

--- public/service-worker.js
+++ public/service-worker.js
@@ -31,3 +31,22 @@ self.addEventListener("fetch", event => {
     )
   );
 });
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+      await self.clients.claim();
+    })()
+  );
+});
